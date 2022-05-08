db6edea87c556c17e3618deb28b2ca3acd850e09
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-05-08 15:11:00 +0100

    text + image added (no styling)

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,20 @@
 import Navbar from "../../components/Navbar/Navbar"
+import Image from "../../components/Image/Image";
+import data from "../../data/text";
+
 
 const About = () => {
   return <>
+  <h2>About us</h2>
+
+  <h3> {data[1].title} </h3>
+  <p>{data[1].paragraph}</p>
+  <p>{data[1].paragraph}</p>
+  <Image /> 
+  <p>{data[0].subtitle}</p>
+  <p>{data[0].paragraph}</p>
+  <p>{data[1].paragraph}</p>
+
 
   </>
 }
