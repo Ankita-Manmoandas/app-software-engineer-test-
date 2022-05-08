b6f0277e651c7a0a31bc001e7b8104c7baa81953
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-05-08 16:37:34 +0100

    Yup validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,94 +1,89 @@
-import React from 'react';
- import { useFormik } from 'formik';
- 
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
- const validate = values => {
-  const errors = {};
-  if (!values.fullName) {
-    errors.fullName = 'Required';
-  } else if (values.fullName.length < 5) {
-    errors.fullName = 'Must be 5 characters or more';
-  }
+const Form = () => {
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      message: "",
+    },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .min(5, 'Must be 5 characters or more')
+        .required('Required'),
+      phoneNumber: Yup.number()
+        .max(9, 'Must be 9 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+      message: Yup.string()
+      .min(5, 'Must be 5 characters or more')
+      .required('Required')
 
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="fullName"> Full Name</label>
+      <input
+        id="fullName"
+        name="fullName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.fullName}
+      />
+      {formik.touched.fullName && formik.errors.fullName ? (
+        <div>{formik.errors.fullName}</div>
+      ) : null}
 
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  return errors;
-};
-
- const Form = () => {
-   // Note that we have to initialize ALL of fields with values. These
-   // could come from props, but since we don’t want to prefill this form,
-   // we just use an empty string. If we don’t do this, React will yell
-   // at us.
-   const formik = useFormik({
-     initialValues: {
-       fullName: '',
-       phoneNumber: '',
-       email: '',
-       message: '',
-     },
-     validate,
-     onSubmit: values => {
-       alert(JSON.stringify(values, null, 2));
-     },
-   });
-   return (
-     <form onSubmit={formik.handleSubmit}>
-       <label htmlFor="fullName"> Full Name</label>
-       <input
-         id="fullName"
-         name="fullName"
-         type="text"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.fullName}
-       />
-          {formik.touched.fullName && formik.errors.fullName ? ( <div>{formik.errors.fullName}</div>) : null}
- 
- 
- 
-       <label htmlFor="email">Email Address</label>
-       <input
-         id="email"
-         name="email"
-         type="email"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.email}
-       />
-        {formik.touched.email && formik.errors.email ? (<div>{formik.errors.email}</div>): null}
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <label htmlFor="phoneNumber"> Phone Number </label>
-       <input
-         id="phoneNumber"
-         name="phoneNumber"
-         type="number"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.phoneNumber}
-       />
-        {formik.touched.phoneNumber && formik.errors.phoneNumber ? (<div>{formik.errors.phoneNumber}</div>) : null}
+      <input
+        id="phoneNumber"
+        name="phoneNumber"
+        type="number"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.phoneNumber}
+      />
+      {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+        <div>{formik.errors.phoneNumber}</div>
+      ) : null}
 
-<label htmlFor="message">Message</label>
-       <input
-         id="message"
-         name="message"
-         type="text"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.message}
-       />
-        {formik.touched.message && formik.errors.message ? (<div>{formik.errors.message}</div>) : null}
- 
-       <button type="submit">Submit</button>
-     </form>
-   );
- };
+      <label htmlFor="message">Message</label>
+      <input
+        id="message"
+        name="message"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.message}
+      />
+      {formik.touched.message && formik.errors.message ? (
+        <div>{formik.errors.message}</div>
+      ) : null}
 
- export default Form; 
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
