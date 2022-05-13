5b9504c74eafd2254ec1f57ef7ef0ce9cb07068a
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-05-13 21:57:45 +0100

    carousel scss overlay added

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -27,13 +27,18 @@ const Carousel = (props) => {
   return (
     <div className="carousel">
       <div className="carousel__icons">
+        <section className ="carousel__overlay">
+      
         <img
           src={leftArrow}
           alt="left arrow"
           onClick={handleDecrement}
           className="carousel__arrow carousel__arrow--left"
         />
+        
+
         <img src={imagesArr[counter]} alt="" className="carousel__image" />
+       
 
         <img
           src={rightArrow}
@@ -41,6 +46,7 @@ const Carousel = (props) => {
           onClick={handleIncrement}
           className="carousel__arrow carousel__arrow--right"
         />
+          </section>
       </div>
 
       <div className="carousel__text">
