f0a52cc0f9361b06cc1ef27a38890f80bafda0eb
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-05-15 16:06:57 +0100

    git deployment

--- src/components/NavMenu/Navmenu.jsx
+++ src/components/NavMenu/Navmenu.jsx
@@ -10,15 +10,15 @@ const Navmenu = (props) => {
     <div className="nav-menu">
       <div className="nav-menu__content">
         <img src={blackCross} onClick={toggle} className="cross" />
-        <Link to="/" className="nav-menu__content__item" onClick={toggle}>
+        <Link to="" className="nav-menu__item" onClick={toggle}>
           Home
         </Link>
-        <Link to="/about" className="nav-menu__content__item" onClick={toggle}>
+        <Link to="/about" className="nav-menu__item" onClick={toggle}>
           About
         </Link>
         <Link
           to="/contact"
-          className="nav-menu__content__item"
+          className="nav-menu__item"
           onClick={toggle}
         >
           Contact
